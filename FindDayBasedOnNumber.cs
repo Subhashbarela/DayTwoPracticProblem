3ad6d7a2bd1b4c84d10df9a20f00b8323ffd3d3e
Subhash Barela <barelasubhash16@gmail.com>
2023-04-28 11:55:54 +0530

    Find Days using Switch Case

---
+++ FindDayBasedOnNumber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwoCsharpBasicPracticProblem
+{
+    public class FindDayBasedOnNumber
+    {
+        public void WeekDays()
+        {
+            Console.WriteLine("Please insert the number as u want to print Day");
+            int day = int.Parse(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Today is Sunday");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Today is Monday");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Today is Tuesday");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Today is Wednesday");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Today is Thursday");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Today is Friday");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Today is Saturday");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine(" user insert invalid number of day");
+                        break;
+                    }
+            }
+        }
+    }
+}
