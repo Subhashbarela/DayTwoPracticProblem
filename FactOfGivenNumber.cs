8fe35f90dedef6d6151574c65fc2d53753ca8820
Subhash Barela <barelasubhash16@gmail.com>
2023-04-28 12:24:18 +0530

    find the factorial number given range

---
+++ FactOfGivenNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwoCsharpBasicPracticProblem
+{
+    public class FactOfGivenNumber
+    {
+        public void FactorialOfGivenNumber(int num)
+        {
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("factorial of given number is: " + fact);
+        }
+      
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,16 @@ namespace DayTwoCsharpBasicPracticProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Csharp Basic Practic Program");
+            FactOfGivenRange fact = new FactOfGivenRange();
+            Console.WriteLine("Please insert the range to find fact :");
+            int num=int.Parse(Console.ReadLine());
+            fact.FactOfGivenRange_Method(num);
+
+            FactOfGivenNumber number= new FactOfGivenNumber();
+            Console.WriteLine("Please insert the number to find fact :");
+            int num2 = int.Parse(Console.ReadLine());
+            number.FactorialOfGivenNumber(num2);
+            Console.ReadLine();
         }
     }
 }
