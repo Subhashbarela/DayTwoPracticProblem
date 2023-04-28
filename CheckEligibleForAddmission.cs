213c1a56679d2b9bb51ad9ac3fc30ef16ac8d9da
Subhash Barela <barelasubhash16@gmail.com>
2023-04-28 11:46:58 +0530

    check student is eligible for addmission or not

---
+++ CheckEligibleForAddmission.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwoCsharpBasicPracticProblem
+{
+    public class CheckEligibleForAddmission
+    {
+        public bool EligibleOrNot(int math,int physics,int chem)
+        {
+            
+            if (math + physics + chem >= 180)
+            {
+                return true;
+            }
+            else if ((math + chem) >= 140)
+            {
+                return true;         
+            }
+            else if ((math + physics) >= 140)
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
